420d9f369b5e10a6099cca066b90323aa6df9b96
mikehakmann <mikehakholm@gmail.com>
2022-10-13 14:41:06 +0200

    Added control over two cannons
    and comments

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,62 +1,93 @@
 using System.Collections;
-using System;
-using System.Collections.Generic;
-using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 
-    public class Shooting : MonoBehaviour
+
+public class Shooting : MonoBehaviour
+{
+    public PlayerInputActions inputActions;
+    public Rigidbody2D CannonBall;
+    public Transform[] FireTransformsLeft;                                                // Refers to the FireTransform that are attatched to the players left
+    public Transform[] FireTransformsRight;                                               // Refers to the FireTransform that are attatched to the players right
+    private bool isShooting;                                                              // Tells if we are shooting at all
+    private bool isShootingLeft;                                                          // Tells if we are shooting left or not
+    private bool isShootingRight;                                                         // Tells if we are shooting right or not
+    public float cannonBallForce = 10f;                                                   // The speed of the cannonball
+    public float fireRateInSeconds = 1f;                                                  // Firerate
+
+    private void Start()
     {
-        public PlayerInputActions inputActions;
-        public Rigidbody2D CannonBall;            
-        public Transform[] FireTransforms;                                                  // Refers to the FireTransform that are attatched to the players lift and right
-        private bool isShooting;                                                            // Tells if we are shooting or not
-        public float cannonBallForce = 10f;                                                 // The speed of the cannonball
-        public float fireRateInSeconds = 1f;                                                // Firerate
-   
-        private void Start()
-        {
-            StartCoroutine(ShootingCoroutine());                                            // Starts a coroutine for shooting
-        }
+        StartCoroutine(ShootingCoroutine());                                              // Starts a coroutine for shooting
+                                                                                       
+    }
 
-        //Instantiates a bullet and applies a force
-        private void Shoot()
+    //Instantiates a bullet and applies a force
+    private void ShootLeft()
+    {
+                                                                                          // If ShootLeft() is called, then ShootCannon for all FireTransforms
+        foreach (var FireTransform in FireTransformsLeft)                                 // in FireTransformLEft
         {
-            isShooting = true;                                                              // If Shoot() is called, then isShooting is true. aka we shoot
-            foreach (var FireTransform in FireTransforms)
-            {
-                Rigidbody2D shellInstance = Instantiate(                                    // Makes the variable  shellInstance equal to the instantiation of the 
-                                                    CannonBall,                             // CannonBall sprite with the position and rotation of FireTransform
-                                                    FireTransform.position,                 //
-                                                    FireTransform.rotation) 
+            ShootCannon(FireTransform);                                                                  
+        }
+    }
+    private void ShootRight()
+    {
+        // If Shoot() is called, then isShooting is true. aka we shoot
+        foreach (var FireTransform in FireTransformsRight)
+        {
+            ShootCannon(FireTransform);                                                                  
+        }
+    }
+    void ShootCannon(Transform FireTransform)
+    {
+        
+        Rigidbody2D cannonBallInstance = Instantiate(                                       // Makes the variable  shellInstance equal to the instantiation of the 
+                                                    CannonBall,                             // CannonBall Rigidbody with the position and rotation of FireTransform
+                                                    FireTransform.position,                 
+                                                    FireTransform.rotation)
                                                     as Rigidbody2D;
-                shellInstance.velocity = cannonBallForce * 1.5f *  FireTransform.up;        // Gives shellInstance a velocity and and a direction
-            }
-        }
+        cannonBallInstance.velocity = cannonBallForce * 1.5f * FireTransform.up;            // Gives shellInstance a velocity and and a direction
+    }
 
-        //Called when fire key(s) is pressed
-        public void OnFire(InputAction.CallbackContext context)
+    //Called when fire key(s) is pressed
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+
+        if (context.performed && value < 0)
+        {                                                                                   // context.performed means whilst the button is pressed down
+            isShootingLeft = true;                                                          // .start would be when the button is first pressed and
+            Debug.Log("Shot left");                                                         // .canceled would be at the release of the button
+}                                                                                   
+        else if (context.performed && value > 0)
         {
-            if (context.performed)                                                          // context.performed means whilst the button is pressed down
-                isShooting = true;                                                          // .start would be when the button is first pressed and
-            else                                                                            // .canceled would be at the release of the button
-                isShooting = false;
+            isShootingRight = true;
+            Debug.Log("Shot right");
         }
+        else
+        {                                                                                    
+            isShootingLeft = false;
+            isShootingRight = false;
+        }
+    }                                                                              
 
         //If holding firekey shoot every fireRate
         IEnumerator ShootingCoroutine()                                                     // Coroutine called at start
-        {                                                                                   // 
-            isShooting = false;                                                             // isShooting is false since we dont wanna shoot when player spawns
+        {                                                                                   
             while (true)                                                                    // Uses a unending while loop to check if we are shooting throughout the
             {                                                                               // game
-                if (isShooting)
-                {                                                                           // If we are shooting, then call shoot() and wait an amount equal to fireRate
-                    Shoot();
+                if (isShootingLeft)
+                {                                                                           // If we are shooting, then call ShootLeft() and wait an amount equal to fireRate
+                    ShootLeft();
                     yield return new WaitForSeconds(fireRateInSeconds);
-                    
-                }
+
+                } else if (isShootingRight){                                                // The same but for ShootRight();
+
+                ShootRight();
+                yield return new WaitForSeconds(fireRateInSeconds);
+
+            }
                                                                                             // else return nothing
                 else
                     
@@ -65,6 +96,7 @@ using UnityEngine.UI;
         }
 
 }
+
 
 
 
