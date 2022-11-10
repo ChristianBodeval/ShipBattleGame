2cb4e3674b62e0d5d0bde1282e0d88c1082ec8f9
Marty Rasmussen <32476705+DragonBornESV@users.noreply.github.com>
2022-11-10 11:23:25 +0100

    ui
    hhh

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
@@ -10,25 +11,35 @@ public class TimerScript : MonoBehaviour
     private int         minutes;                                                    // A variable used to hold the time left, (but only the minuts)
     private int         seconds;                                                    // A variable used to show the time left, (but only the seconds)
     public  TMP_Text    timerUI;                                                    // A variable used to hold a refrens to the text UI that shows the timer
-    private float       timeElapced;                                                // A variable used to hold the time sins the program was started when the timerUI is loaded
-
+    private float       timeElapsed;                                                // A variable used to hold the time sins the program was started when the timerUI is loaded
+    public Slider       slider;
     void Start()                                                                    // Start is called before the first frame update
     {
-        roundLenghtSeconds = 240;                                                   // The length of the round (in seconds) is defined
-        timerUI = GetComponent<TMP_Text>();                                         // The Timer_Text component is asigned to the timerUI variable
-        countDownTimer = roundLenghtSeconds;                                        // The start value for the countDownTimer is asigned to be the lenght of the round
-        timeElapced = Time.time;
+        roundLenghtSeconds  = 70;                                                  // The length of the round (in seconds) is defined
+
+        countDownTimer      = roundLenghtSeconds;                                   // The start value for the countDownTimer is asigned to be the lenght of the round
+        slider.maxValue     = roundLenghtSeconds;
+        slider.value        = 0;
+        timeElapsed         = Time.time;
     }
 
-    void FixedUpdate()                                                              // Update is called once per frame
+    void FixedUpdate()                                                              // FixedUpdate is called once per specified time
     {
-        minutes = (int) countDownTimer/60;                                          // the minuts are calculated by dividing the time left in seconds by 60 and rounding out by casting (int)
-        seconds = (int) countDownTimer%60;                                          // the seconds are calculated by taking the seconds left and using modulus(%) 60.
+        minutes = countDownTimer / 60;                                                // the minuts are calculated by dividing the time left in seconds by 60 and rounding out by casting (int)
+        seconds = countDownTimer % 60;                                                // the seconds are calculated by taking the seconds left and using modulus(%) 60.
 
         if (countDownTimer > 0)                                                     // an if statment is used to stop the countdown when zero(0) is reached
-            countDownTimer = (int) (roundLenghtSeconds - (Time.time - timeElapced));// The time left is calculatet by subtracting the amount of time passed from when the timer UI was called, from the total amount of time sins the game was started and then subtracting that from the lenght of the round in seconds
+            countDownTimer = (int)(roundLenghtSeconds - (Time.time - timeElapsed));// The time left is calculatet by subtracting the amount of time passed from when the timer UI was called, from the total amount of time sins the game was started and then subtracting that from the lenght of the round in seconds
 
-        timerUI.text = minutes.ToString() + ":" + string.Format("{0:00}", seconds); // The timerUI is asigned some text in the form of a String containing the minuts and seconds left. The part of the string containing the seconds is formatted to always show 2 digidets.
+        slider.value = roundLenghtSeconds - countDownTimer;
+
+        if (countDownTimer > 60)
+        {
+            timerUI.text = minutes.ToString() + ":" + string.Format("{0:00}", seconds); // The timerUI is asigned some text in the form of a String containing the minuts and seconds left. The part of the string containing the seconds is formatted to always show 2 digidets.
+
+        } else
+        {
+            timerUI.text = countDownTimer.ToString();
+        }
     }
-
 }
