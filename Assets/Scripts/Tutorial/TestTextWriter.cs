aee43d4a7fb8eef314c94ee4e06bda6e48b1dbb4
mikehakmann <mikehakholm@gmail.com>
2022-11-27 13:38:11 +0100

    Added dash, merchantships, ramming and TP

--- Assets/Scripts/Tutorial/TestTextWriter.cs
+++ Assets/Scripts/Tutorial/TestTextWriter.cs
@@ -9,10 +9,6 @@ public class TestTextWriter : MonoBehaviour
     public int index = 1;
     public float Delay = 0.1f;
     public float waitTimer = 2f;
-    private string fullText;                        // The Text currently displayed
-    private string currentText = "";
-    private bool[] firstText = new bool[20]; //Bool array to make sure the text isnt showing every time update runs, 20 to allow for more text
-    private bool buttonClicked = true;
     private bool moveWAndSKeys = false;
     private bool shotKeyGAndH = false;
     private bool shotKeyPeriodAndMinus = false;
@@ -25,10 +21,12 @@ public class TestTextWriter : MonoBehaviour
     public GameObject Island1;
     public GameObject Island2;
     public GameObject Merchantship;
+    public GameObject Merchantship2;
     public GameObject PointingArrows;
+    public GameObject TPArrows;
     public GameObject TutorialImage;
     public Health healthScript;
-    public Text text1;
+    public Text text1;                              // Text objects for all the text being displayed
     public Text text2;
     public Text text3;
     public Text text4;
@@ -43,6 +41,11 @@ public class TestTextWriter : MonoBehaviour
     public Text text13;
     public Text text14;
     public Text text15;
+    public Text text16;
+    public Text text17;
+    public Text text18;
+    public Text text19;
+    public Text text20;
     private bool moveAAndDKeys;
     private bool moveLeftAndRightKeys;
     private bool moveUpAndDownKeys;
@@ -94,52 +97,72 @@ public class TestTextWriter : MonoBehaviour
                     TutorialImage.gameObject.SetActive(true);
                 }
                 break;
-            case 6:
-                text6.gameObject.SetActive(true);
-                break;
-            case 7:
-                text7.gameObject.SetActive(true);
-                GAndHKey.gameObject.SetActive(true);
-                PeriodAndMinusKey.gameObject.SetActive(true);
-                break;
+           case 6: // Teleporter
+               text6.gameObject.SetActive(true);
+               break; 
+           case 7: // Teleporter 2
+               text7.gameObject.SetActive(true);
+                TPArrows.gameObject.SetActive(true);
+               break;
             case 8:
-                if (shotKeyPeriodAndMinus && shotKeyGAndH)
-                {
-                    text8.gameObject.SetActive(true);
-                    TutorialImage.gameObject.SetActive(true);
-                }
+                text8.gameObject.SetActive(true);
+                TPArrows.gameObject.SetActive(false);
                 break;
             case 9:
                 text9.gameObject.SetActive(true);
+                GAndHKey.gameObject.SetActive(true);
+                PeriodAndMinusKey.gameObject.SetActive(true);
                 break;
             case 10:
-                text10.gameObject.SetActive(true);
+                if (shotKeyPeriodAndMinus && shotKeyGAndH)
+                {
+                    text10.gameObject.SetActive(true);
+                    TutorialImage.gameObject.SetActive(true);
+                }
+                break;
+            case 11: // Ramming
+                text11.gameObject.SetActive(true);
+                break;
+            case 12: // Ramming 2
+                text12.gameObject.SetActive(true);
+                break;
+            case 13: // Dash
+                text13.gameObject.SetActive(true);
+                break;
+            case 14:
+                text14.gameObject.SetActive(true);
+                break;
+            case 15:
+                text15.gameObject.SetActive(true);
                 Island1.gameObject.SetActive(true);
                 Island2.gameObject.SetActive(true);
 
                 break;
-            case 11:
+            case 16:
                 //if (healthScript.dead)
                 if (Island1.active == false && Island2.active == false)
                 { 
-                text11.gameObject.SetActive(true);
+                text16.gameObject.SetActive(true);
                 }
                 break;
-            case 12:
-                text12.gameObject.SetActive(true);
+            case 17:
+                text17.gameObject.SetActive(true);
                 PointingArrows.gameObject.SetActive(true);
 
                 break;
-            case 13:
-                text13.gameObject.SetActive(true);
+            case 18:
+                text18.gameObject.SetActive(true);
 
                 break;
-            case 14:
-                text14.gameObject.SetActive(true);
+            case 19:
+                text19.gameObject.SetActive(true);
                 Merchantship.gameObject.SetActive(true);
+                Merchantship2.gameObject.SetActive(true);
                 break;
-            case 15:
-                text15.gameObject.SetActive(true);
+            case 20:
+                if (Merchantship.active == false && Merchantship2.active == false)
+                    text20.gameObject.SetActive(true);
+
                 break;
             default:
                 Debug.Log("Oops, ran out of text UwU");
@@ -187,7 +210,7 @@ public class TestTextWriter : MonoBehaviour
                 TutorialImage.gameObject.SetActive(false);
             }
         }
-        if (index == 8)
+        if (index == 10)
         {
             if (checker2)
             {
@@ -195,21 +218,21 @@ public class TestTextWriter : MonoBehaviour
                 TutorialImage.gameObject.SetActive(false);
             }
         }
-        if (Input.GetKey(KeyCode.G) && index == 8 ||
-          Input.GetKey(KeyCode.H) && index == 8)
+        if (Input.GetKey(KeyCode.G) && index == 10 ||
+          Input.GetKey(KeyCode.H) && index == 10)
         {
             yield return new WaitForSeconds(1);
             shotKeyGAndH = true;
             GAndHKey.gameObject.SetActive(false);
         }
-        if (Input.GetKey(KeyCode.Period) && index == 8 ||
-          Input.GetKey(KeyCode.Minus) && index == 8)
+        if (Input.GetKey(KeyCode.Period) && index == 10 ||
+          Input.GetKey(KeyCode.Minus) && index == 10)
         {
             yield return new WaitForSeconds(1);
             shotKeyPeriodAndMinus = true;
             PeriodAndMinusKey.gameObject.SetActive(false);
         }
-        if (index == 13)
+        if (index == 18)
         {
             yield return new WaitForSeconds(3);
             PointingArrows.gameObject.SetActive(false);
