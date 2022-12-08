23c8c17fad69dadaf12b42322a31cf34d6fa9f07
Marty Rasmussen <32476705+DragonBornESV@users.noreply.github.com>
2022-12-08 13:23:48 +0100

    så

--- Assets/Scripts/CountDownUI.cs
+++ Assets/Scripts/CountDownUI.cs
@@ -28,10 +28,17 @@ public class CountDownUI : MonoBehaviour
         timerUI.transform.localScale = new Vector3(scalor, scalor, 1);
         countDown = (int) (countDownLength - (Time.time - timeElapsed));
         timerUI.text = countDown.ToString();
-        //timerUI.transform.localScale = new Vector3(0.5f, 0.5f, 1);
-        if (countDown <= 0)
+
+        if (countDown < 1)
         {
-            countDown = 0;
+            timerUI.text = "FIGHT!!!";
+            timerUI.transform.localScale = new Vector3(4f, 4f, 1);
+        }
+
+        //timerUI.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+        if (countDown <= -1)
+        {
+            countDown = -1;
             inGameUI.SetActive(true);
             countDownObject.SetActive(false);
         }
